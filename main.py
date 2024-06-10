697f901ecd8ed9bf5a27fa282eec947ffdcf266e
Denys K <github@kibitkin.com>
2024-06-10 15:32:26 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# Standard Libraries
+import sys
+import traceback
+
+
+def stop():
+    sys.exit(1)
+
+
+def main():
+    try:
+        pass
+    except KeyboardInterrupt:
+        print("[ERROR] Failed: script interrupted by user (CTRL + C)")
+        stop()
+    except Exception as e:
+        print(f"[ERROR] Failed: unexpected exception: {e}")
+        traceback.print_exc()  # traceback included
+
+
+if __name__ == '__main__':
+    main()
