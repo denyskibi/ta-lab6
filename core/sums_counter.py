cfaecec47cf400f1e643e13da747ffc3c8d900fd
Denys K <github@kibitkin.com>
2024-06-10 15:48:39 +0300

    Added sums counter function and print result

--- core/sums_counter.py
+++ core/sums_counter.py
@@ -1,6 +1,19 @@
-class SumsCounter:
-    def __init__(self):
-        pass
+# Standard Libraries
+from typing import Set
 
-    def count(self) -> int:
-        pass
+
+class SumsCounter:
+    @staticmethod
+    def count(numbers: Set[int], min_sum: int, max_sum: int) -> int:
+        unique_sums_count = 0
+
+        for s in range(min_sum, max_sum + 1):
+            # Tests whether there are two distinct numbers x and y such that x + y = S
+            for x in numbers:
+                y = s - x
+
+                if y in numbers and x != y:
+                    unique_sums_count += 1
+                    break
+
+        return unique_sums_count
--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 import sys
 import traceback
 
-# Custom Libraries
+# Custom Modules
 from utils import file_utils
+from core.sums_counter import SumsCounter
 
 
 def stop():
@@ -11,12 +12,16 @@ def stop():
 
 
 def main():
+    # Create necessary class objects
+    sums_counter = SumsCounter()
+
     try:
         # Step #1: Loading 1M numbers from txt file
         loaded_unique_numbers = file_utils.load_unique_numbers_from_txt_file(file_path="files/input_1M_numbers.txt")
 
-        # Step #2: Count unique sums
-        ...
+        # Step #2: Count unique sums & print result
+        unique_sums = sums_counter.count(numbers=loaded_unique_numbers, min_sum=-1000, max_sum=1000)
+        print(f"[INFO] Unique sums count: {unique_sums}")
     except KeyboardInterrupt:
         print("[ERROR] Failed: script interrupted by user (CTRL + C)")
         stop()
